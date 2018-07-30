fc8ea8e7afc012c14aff6f9be4bf88464c8af452
Wesley <wangp_yong@163.com>
2018-07-30 18:11:06 +0800

    寻找Bean实例化的入口

--- springboot-refresh-context/src/main/java/spring/study/refresh/context/bean/SimpleService.java
+++ springboot-refresh-context/src/main/java/spring/study/refresh/context/bean/SimpleService.java
@@ -1,17 +1,21 @@
 package spring.study.refresh.context.bean;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 /**
+ * 在ApplicationContext的refresh方法中的finishBeanFactoryInitialization方法作为逻辑入口, 实例化当前Bean
  * Created by Format on 2017/5/4.
  */
 @Component
 public class SimpleService {
 
     private String name = "simple";
+
+    @PostConstruct
+    public void start(){
+        System.out.println("SimpleService.start()");
+    }
 
     public SimpleService() {
         System.out.println("------ SimpleService constructor invoke");
